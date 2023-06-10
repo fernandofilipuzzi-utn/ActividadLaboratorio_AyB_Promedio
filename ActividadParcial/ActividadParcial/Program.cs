907881242b4e04722a7aaaaf660038578a3d17df
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-06-10 16:40:52 -0300

    actividad #1

--- ActividadParcial/ActividadParcial/Program.cs
+++ ActividadParcial/ActividadParcial/Program.cs
@@ -10,6 +10,59 @@ namespace ActividadParcial
     {
         static void Main(string[] args)
         {
+            //instanciación clase de gestión del promedio
+            Promedio p = new Promedio();
+
+            int op;
+            do 
+            {
+                #region pantalla de menú
+                Console.WriteLine("\t\tCálculo del promedio");
+                Console.WriteLine("1- Ingresar valor");
+                Console.WriteLine("2- Calcular y Mostrar promedio");
+                Console.WriteLine("Otro- Salir");
+                op = Convert.ToInt32(Console.ReadLine());
+                #endregion
+
+                #region procesando las demás pantallas
+                switch (op)
+                {
+                    case 1:
+                        {
+                            #region pantalla de ingreso de datos
+
+                            Console.WriteLine("Ingrese el valor");
+                            int valor = Convert.ToInt32( Console.ReadLine() );
+                            p.AgregarValor(valor);
+
+                            Console.WriteLine("Presione cualquier tecla para volver al menú.");
+                            Console.ReadKey();
+
+                            #endregion
+                        }
+                        break;
+                    case 2:
+                        {
+                            #region pantalla para mostrar datos
+                            
+                            double promedio = p.CalcularPromedio();
+                            Console.WriteLine("El promedio es {0:f2}", promedio);
+
+                            Console.WriteLine("Presione cualquier tecla para volver al menú.");
+                            Console.ReadKey();
+
+                            #endregion
+                        }
+                        break;
+                    default: 
+                        {
+                            op = 0;
+                        }break;
+                }
+                #endregion
+            } while (op>0);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ActividadParcial/ActividadParcial/Promedio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadParcial
+{
+    class Promedio
+    {
+        double acum;
+        int cnt;
+
+        public Promedio()
+        {
+            acum = 0;
+            cnt = 0;
+        }
+
+        public void AgregarValor(double valor)
+        {
+            acum += valor;
+            cnt++; 
+        }
+        public double CalcularPromedio()
+        {
+            double promedio = 0;
+            if(cnt>0)
+                promedio=acum/cnt;
+            return promedio;
+        }
+    }
+}
